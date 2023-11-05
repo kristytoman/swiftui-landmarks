1f9477bdb83e7598a9afee9c115a101dbbd63d15
Kristýna Tomanová <kristytoman@proton.me>
2023-11-05 20:25:04 +0100

    Add learning comments

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
-
+// By default, SwiftUI view files declare a structure and a preview.
 struct ContentView: View {
+    // The structure conforms to the View protocol
+    // and describes the view’s content and layout.
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +22,8 @@ struct ContentView: View {
 }
 
 #Preview {
+    // The preview declaration creates a preview for that view.
+    // The canvas displays a preview automatically.
+    // If the canvas isn’t visible, select Editor > Canvas to show it.
     ContentView()
 }
--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 @main
+// The @main attribute identifies the app’s entry point.
 struct LandmarksApp: App {
+    // An app that uses the SwiftUI app life cycle has a structure
+    // that conforms to the App protocol.
     var body: some Scene {
+        // The structure’s body property returns one or more scenes,
+        // which in turn provide content for display
         WindowGroup {
             ContentView()
         }
