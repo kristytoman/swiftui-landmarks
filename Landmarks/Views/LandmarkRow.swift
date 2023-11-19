13a6f652c475882b23084a328c866e431935488b
Kristýna Tomanová <kristytoman@proton.me>
2023-11-19 17:53:50 +0100

    [Feature] Create landmark row

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Kristýna Tomanová on 19.11.2023.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
