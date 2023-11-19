243e5f7db260fb61aba9ea756e031f54b5f82080
Kristýna Tomanová <kristytoman@proton.me>
2023-11-19 18:28:41 +0100

    [Refactor] Add scrollView and Navigation details to LandmarkDetail

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -11,37 +11,42 @@ struct LandmarkDetail: View {
     var landmark: Landmark
     
     var body: some View {
-        VStack {
-            MapView(coordinates: landmark.locationCoordinates)
-                .frame(height: 300)
-            
-            CircleImage(image: landmark.image)
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
+        ScrollView {
             VStack {
-                VStack(alignment: .leading) {
-                    Text(landmark.name)
-                        .font(.title)
-                    HStack {
-                        Text(landmark.park)
-                            .font(.subheadline)
-                        Spacer()
-                        Text(landmark.state)
-                            .font(.subheadline)
+                MapView(coordinates: landmark.locationCoordinates)
+                    .frame(height: 300)
+                
+                CircleImage(image: landmark.image)
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                
+                VStack {
+                    VStack(alignment: .leading) {
+                        Text(landmark.name)
+                            .font(.title)
+                        HStack {
+                            Text(landmark.park)
+                            Spacer()
+                            Text(landmark.state)
+                        }
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                        
+                        Divider()
+                        
+                        Text("About \(landmark.name)")
+                            .font(.title2)
+                        Text(landmark.description)
+                        
                     }
-                    Divider()
-                    
-                    Text("About \(landmark.name)")
-                        .font(.title2)
-                    Text(landmark.description)
-                    
                 }
+                .padding()
+                
+                Spacer()
             }
-            .padding()
-            
-            Spacer()
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
