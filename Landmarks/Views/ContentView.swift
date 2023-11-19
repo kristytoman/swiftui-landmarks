6c519e69ec7f57e9a3eef5783fb2fbfac475da57
Kristýna Tomanová <kristytoman@proton.me>
2023-11-19 18:07:41 +0100

    [Feature] Create landmark detail

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -11,39 +11,7 @@ struct ContentView: View {
     // The structure conforms to the View protocol
     // and describes the view’s content and layout.
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack {
-                VStack(alignment: .leading) {
-                    Text("Turtle Rock")
-                        .font(.title)
-                    HStack {
-                        Text("Joshua Tree National Park")
-                            .font(.subheadline)
-                        Spacer()
-                        Text("California")
-                            .font(.subheadline)
-                        
-                        
-                    }
-                    Divider()
-                    
-                    Text("About Turtle Rock")
-                        .font(.title2)
-                    Text("Descriptive text goes here.")
-                    
-                }
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
