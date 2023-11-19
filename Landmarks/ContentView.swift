05e41919cf6c479447857e73689634a64372208e
Kristýna Tomanová <kristytoman@proton.me>
2023-11-19 17:01:02 +0100

    [Feature] Add MapView to the ContentView

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -12,19 +12,24 @@ struct ContentView: View {
     // and describes the view’s content and layout.
     var body: some View {
         VStack {
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-            }.padding()
+            MapView()
+                .frame(height: 300)
+            
+            VStack {
+                VStack(alignment: .leading) {
+                    Text("Turtle Rock")
+                        .font(.title)
+                    HStack {
+                        Text("Joshua Tree National Park")
+                            .font(.subheadline)
+                        Spacer()
+                        Text("California")
+                            .font(.subheadline)
+                    }
+                }.padding()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
